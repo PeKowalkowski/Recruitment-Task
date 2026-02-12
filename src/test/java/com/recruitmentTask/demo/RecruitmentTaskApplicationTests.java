2706cbc218ffb4c6af286fc06055accfc27d6002
Paweł Kowalkowski <pekowalkowski@gmail.com>
2026-02-12 17:51:21 +0100

    first commit

---
+++ src/test/java/com/recruitmentTask/demo/RecruitmentTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recruitmentTask.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
